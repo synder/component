4dfc712a41630aeb2f56a4a284d03e74a4f98bac
synder <812256717@qq.com>
2016-04-26 15:29:06 +0800

    add

--- controller/home/index.js
+++ controller/home/index.js
@@ -7,10 +7,23 @@
 var apiRequester = require('../../helper/requestHelper').apiRequester;
 
 exports.homeIndex = function(req, res, next){
-    var i = Math.random() * 50 + 50;
+    var i = Math.round(Math.random() * 1000 + 250);
 
-   setTimeout(function(){
-       res.render('home/index');
-   }, i);
+    if(i % 7 == 0){
+        throw new Error('x');
+    }
 
+    if(i % 2 == 0){
+        var buffer = new Array(1000000);
+        for(var x = 0 ; x < 1000000; x++){
+            buffer[x] = x;
+        }
+        return res.render('home/index');
+    }
+
+    if(i % 3 == 0){
+        setTimeout(function(){
+            res.render('home/index');
+        }, i);
+    }
 };
